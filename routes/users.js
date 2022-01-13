766e73e74ea6aa940e7c21f62c5c6023ec81f561
Irene San José <75340773+irenesj@users.noreply.github.com>
2022-01-13 18:29:06 +0100

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,35 @@ const res = require('express/lib/response');
 var router = express.Router();
 
 /* Array to save users */
-var users = [];
+var users = [
 
-/* GET users listing. */
+  {
+    name: "usuario1",
+    surname: "apellidosUsuario1",
+    phone: "11111111",
+    mail: "usuario1@mail.com",
+    password: "udfgsdufg",
+    city: "Madrid",
+    community: "Madrid",
+    Province: "Madrid",
+    postalCode: "28000"
+
+  },
+  {
+    name: "usuario2",
+    surname: "apellidosUsuario2",
+    phone: "11111111",
+    mail: "usuario2@mail.com",
+    password: "hcbdjfjbhkj",
+    city: "Orense",
+    community: "Galicia",
+    Province: "Orense",
+    postalCode: "29000"
+
+  }
+];
+
+/* GET */
 router.get('/', function(req, res, next) {
 
   const response = users.map(userName => {
@@ -17,7 +43,7 @@ router.get('/', function(req, res, next) {
 
 });
 
-/* POST insert new user. */
+/* POST */
 router.post('/', function (req, res) {
  
  var name = req.body.name || '';
