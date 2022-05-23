32e523fe6ff37c4da70ea76cbb3dc7cf09e9ff1c
Irene San José <75340773+irenesj@users.noreply.github.com>
2022-05-23 12:52:08 +0200

    More corrections and comments added

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,6 @@ const res = require('express/lib/response');
 const router = express.Router();
 const users = require('../data/usersData');
 const tokens = require('../data/tokensData'); // const: la referencia no se cambia, es decir, no se puede apuntar a otro sitio
-const filter = require('jade/lib/filters');
   
   
 // This function generates a token with 40 random letters and numbers
@@ -24,28 +23,33 @@ const generateToken = () => {
 /** LOGIN */
 router.post('/', function(req, res){
 
+  // We take the mail and the password of the user that wants to log
   const { mail, password } = req.body;
 
-console.log(users);
-
+  // Then we search in the database is there´s an user with that mail and that password
   const arrayResult = users.filter(user => user.mail.toLowerCase() == mail.toLowerCase() && user.password == password); 
   
+  // If there's none
   if(arrayResult.length == 0){
 
     res.status(401).json({error: 'Datos erróneos. Inténtalo de nuevo.'});
 
   }
-  else {
+  else { // If there is
 
+    // We generate a random token ...
     const token = generateToken();
+    // ... and assign that token to the user's mail: example "876578gfhjrfb755868": "userLogged@mail.com"
     tokens[token] = arrayResult[0].mail;
     res.status(200).setHeader('Location', `http://localhost:3001/login`);
 
+    // Now we make a copy of the logged user to be able to send the response without his/her password for security reasons
     const userCopy = {
       ...arrayResult[0]
     }
     delete userCopy["password"];
     
+    // Then we send the token and the user to the client
     res.json({ token: token, user: userCopy });
 
   }
@@ -55,7 +59,9 @@ console.log(users);
 /** LOGOUT */
 router.delete('/', function(req, res, next){
 
+  // We ibtain the user's token ...
   const token = req.headers.authorization.substring("Bearer ".length);
+  // ... and then delete it from the tokens in the database
   delete tokens[token];
   res.status(200);
   res.json({
